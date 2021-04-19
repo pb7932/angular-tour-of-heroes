1284fcca5adba466782f9ca8243d52510d4a2b0a
pb7932 <pebe1928@gmail.com>
2021-04-19 18:05:17 +0200

    Creating a in-memory-data service to get heroes over http

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
@@ -9,8 +10,10 @@ import { MessageService} from './message.service';
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes'
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient,
+              private messageService: MessageService) { }
 
   getHero(id: number):Observable<Hero> {
     const hero = HEROES.find(h => h.id === id) as Hero;
@@ -19,9 +22,16 @@ export class HeroService {
   }
 
   getHeroes(): Observable<Hero[]> {
+    /*
     const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
     return heroes;
+    */
+   return this.http.get<Hero[]>(this.heroesUrl);
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 
 }
