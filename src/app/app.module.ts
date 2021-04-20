07fd67b1ea193c40392ed96a59a4dbfc4d6965d1
pb7932 <pebe1928@gmail.com>
2021-04-20 18:26:32 +0200

    Refactoring the hero feature by adding the hero module and hero routing module, and moving everything into a heroes folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './services/in-memory-data.service';
-
+import { InMemoryDataService } from './heroes/services/in-memory-data.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './components/heroes/heroes.component';
-import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
-import { MessagesComponent } from './components/messages/messages.component';
+
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { HeroSearchComponent } from './components/hero-search/hero-search.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { HeroesComponent } from './heroes/heroes/heroes.component';
+import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { HeroSearchComponent } from './heroes/hero-search/hero-search.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
-    HeroDetailComponent,
     MessagesComponent,
     DashboardComponent,
     HeroSearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false })
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false }),
+    HeroesModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/services/hero.service.ts
+++ src/app/heroes/services/hero.service.ts
@@ -5,7 +5,7 @@ import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
-import { MessageService} from './message.service';
+import { MessageService} from '../../services/message.service';
 import { templateJitUrl } from '@angular/compiler';
 
 @Injectable({
